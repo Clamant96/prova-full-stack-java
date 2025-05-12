3fce8cf7c8fd597617099c14c9b5d36173f9b808
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2025-05-12 08:57:27 -0300

    Ajuste de configuracao para renderizacao de suwagger

--- provaFullStackJava/src/main/java/br/com/helpconnect/provaFullStackJava/security/BasicSecurityConfig.java
+++ provaFullStackJava/src/main/java/br/com/helpconnect/provaFullStackJava/security/BasicSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -19,9 +18,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.MethodArgumentBuilder;
-
-import aj.org.objectweb.asm.commons.Method;
 
 @Configuration
 @EnableWebSecurity
@@ -64,21 +60,24 @@ public class BasicSecurityConfig {
             .cors(withDefaults())
             .authorizeHttpRequests(auth -> auth
         		.requestMatchers(
-                        "/",
-                        "/index.html",
-                        "/*.js",
-                        "/*.css",
-                        "/*.ico",
-                        "/assets/**",
-                        "/img/**"
-                    ).permitAll()
+                    "/",
+                    "/index.html",
+                    "/*.js",
+                    "/*.css",
+                    "/*.ico",
+                    "/assets/**",
+                    "/img/**"
+                ).permitAll()
         		.requestMatchers(
-                        "/swagger-ui/**",
-                        "/v3/api-docs/**",
-                        "/swagger-resources/**",
-                        "/swagger-ui.html",
-                        "/webjars/**"
-                    ).permitAll()
+    			    "/v3/api-docs/**",
+    			    "/swagger-ui/**",
+    			    "/swagger-ui.html"
+    			).permitAll()
+        		.requestMatchers(
+                    "/v3/api-docs/**",
+                    "/swagger-ui/**",
+                    "/swagger-ui.html"
+                ).permitAll()
                 .requestMatchers("/usuario/login").permitAll()
                 .requestMatchers("/usuario/cadastrar").permitAll()
                 .requestMatchers("/actuator/health").permitAll()
